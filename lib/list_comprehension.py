26e31b0e31ed3be415ad8d93624658dd832987c5
github username <github email address>
2022-07-11 09:48:40 -0400

    done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3 
 
 def return_evens(num_list):
-    pass
+    return [n for n in num_list if n % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    eg = (word + "!" for word in sentence_list)
+    return [] if len(sentence_list) == 0 else list(eg)
